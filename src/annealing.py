e48b395a73121b2badd0347ab6f70b14ed235414
Theomat <theomatricon@gmail.com>
2021-01-06 12:49:16 +0100

    Added a translated annealing

--- src/annealing.py
+++ src/annealing.py
@@ -15,3 +15,7 @@ def linear(start: float, end: float, duration: int) -> Annealing:
 
 def exponential(start: float, end: float, decay: float) -> Annealing:
     return lambda i: start + (end - start) * np.exp(- i / decay)
+
+
+def translated(translation: int, annealing: Annealing) -> Annealing:
+    return lambda i: annealing(i + translation)
