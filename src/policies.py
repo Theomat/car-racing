460f48c9b50aff4555787f8627ca7aad3045a6ae
Theomat <theomatricon@gmail.com>
2021-01-06 17:03:27 +0100

    Added function from_model

--- src/policies.py
+++ src/policies.py
@@ -1,5 +1,7 @@
 from typing import Callable
 import numpy as np
+import torch
+import torch.nn as nn
 
 Policy = Callable[[np.ndarray], int]
 TrainingPolicy = Callable[[np.ndarray, int, int], int]
@@ -13,3 +15,11 @@ def to_epsilon_greedy(policy: Policy, epsilon: Callable[[int], float], action_sp
         else:
             return policy(state)
     return training_policy
+
+
+def from_model(model: nn.Module) -> Policy:
+    def policy(state: np.ndarray) -> int:
+        state_tensor = torch.transpose(torch.from_numpy(state), 0, -1).float()
+        ordered_q_values = model(state_tensor.unsqueeze(0)).argsort(descending=True)[0]
+        return ordered_q_values[0].item()
+    return policy
