832dde014ceb0946b0bdb17884ba0fc94c35ae23
Theomat <theomatricon@gmail.com>
2021-01-22 17:42:58 +0100

    DQN model

--- src/policy_model.py
+++ src/policy_model.py
@@ -1,57 +1,23 @@
 import torch.nn as nn
-import torch.nn.functional as F
-
-CONVOLUTIONAL_FILTERS = 32
 
 
-class ResidualLayer(nn.Module):
-    def __init__(self):
-        super(ResidualLayer, self).__init__()
+class PolicyModel(nn.Module):
+    def __init__(self, action_space: int, frames_stack: int):
+        super(PolicyModel, self).__init__()
 
         self.layers = nn.Sequential(
-            nn.Conv2d(CONVOLUTIONAL_FILTERS, CONVOLUTIONAL_FILTERS, 3),
-            nn.BatchNorm2d(CONVOLUTIONAL_FILTERS),
+            nn.Conv2d(frames_stack, 6, 7),
             nn.ReLU(),
-            nn.Conv2d(CONVOLUTIONAL_FILTERS, CONVOLUTIONAL_FILTERS, 3),
-            nn.BatchNorm2d(CONVOLUTIONAL_FILTERS),
-            nn.ReLU()
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(6),
+            nn.Conv2d(6, 12, 4),
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(12),
+            nn.Flatten(1),
+            nn.Linear(5292, 216),
+            nn.ReLU(),
+            nn.Linear(216, action_space)
         )
 
     def forward(self, x):
         return self.layers(x)
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, blocks=9):
-        super(ResidualBlock, self).__init__()
-        self.layers = nn.ModuleList([ResidualLayer() for _ in range(blocks)])
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-            x = F.relu(x)
-        return x
-
-
-class PolicyModel(nn.Module):
-    def __init__(self, action_space: int):
-        super(PolicyModel, self).__init__()
-
-        self.initial = nn.Sequential(
-            nn.Conv2d(4, 32, 3),
-            nn.ReLU()
-        )
-        blocks: int = 2
-        self.residual_block = ResidualBlock(blocks)
-        size: int = 96 - (1 + blocks) * 4 + 2
-        self.layers = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(32 * size * size, action_space),
-            nn.ReLU(),
-            nn.Linear(action_space, action_space)
-        )
-
-    def forward(self, x):
-        y = self.initial(x)
-        y = self.residual_block(y)
-        return self.layers(y)
