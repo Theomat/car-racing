0f25e2415b89fc9ada3efd7f74038f7d0a4ef1ac
Theomat <theomatricon@gmail.com>
2021-01-28 17:26:34 +0100

    Update DQN for TF

--- src/dqn.py
+++ src/dqn.py
@@ -1,33 +1,14 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten
 
 
-class DQN(nn.Module):
-
-    def __init__(self, input_channels, input_size, output_size):
-
-        super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=4, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-
-        # Calculate the size of the image when squashed to a linear vector
-        # We assume here that the input image is square
-        conv_length = self._conv_shape(
-            self._conv_shape(self._conv_shape(input_size, 4, 4), 4, 2), 3, 1
-        )
-        conv_shape = conv_length ** 2 * 64
-        self.linear1 = nn.Linear(conv_shape, 512)
-        self.linear2 = nn.Linear(512, output_size)
-
-    @staticmethod
-    def _conv_shape(input_size, filter_size, stride, padding=0):
-        return 1 + (input_size - filter_size + 2 * padding) // stride
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.linear1(x.view(x.size(0), -1)))
-        return self.linear2(x)
+def DQN(self, input_channels, input_size, output_size):
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(4, 4), strides=4, activation='relu',
+                     input_shape=(input_size, input_size, input_channels)))
+    model.add(Conv2D(filters=64, kernel_size=(4, 4), strides=2, activation='relu'))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=1, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(output_size, activation=None))
+    return model
