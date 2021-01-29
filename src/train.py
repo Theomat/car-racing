1119a24223f207ae6d6ea4d5c9f2c0d934d026e2
Theomat <theomatricon@gmail.com>
2021-01-29 09:31:04 +0100

    Added done/not done

--- src/train.py
+++ src/train.py
@@ -84,13 +84,17 @@ def optimize_model(call):
     batch = Transition(*zip(*transitions))
 
     state_batch = torch.stack(batch.state)
-    next_state_batch = torch.stack(batch.next_state)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
     state_action_values = policy_net(state_batch).gather(1, action_batch)
 
-    next_state_values = policy_net(next_state_batch).max(1)[0].detach()
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = policy_net(non_final_next_states).max(1)[0].detach()
 
     # Compute the expected Q values
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
@@ -150,6 +154,8 @@ for episode in tqdm(range(EPISODES)):
 
         state = torch.roll(state, -1, 0)
         state[-1, :, :] = torchfy(observation)
+        if done:
+            state = None
 
         memory.push(prev_state, action, state, reward)
 
